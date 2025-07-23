2c66ca642eb4cdb5e48f79d754d7ce04a09ce476
jpc <jpc@MacBook-Air.local>
2025-07-23 08:38:58 +0800

    heroku try fix deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- VULNERABILITY: Using outdated middleware ---
 // Older versions of Express and its middleware have known vulnerabilities.
